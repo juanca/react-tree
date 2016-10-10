1d1fa84921b817035fed11050d0ab6989cadd1c6
Juan Carlos Medina <juanca.med@gmail.com>
2016-10-09 19:31:34 -0700

    npm install --save babel-loader babel-core babel-preset-es2015 babel-preset-react

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,16 @@ module.exports = {
   externals: {
     'react': 'React'
   },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }],
+  },
   output: {
     path: __dirname,
     filename: 'react-tree.js',
