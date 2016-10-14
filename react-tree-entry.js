9637114311eb3552dcf61baa380dc6e33d058be4
Juan Carlos Medina <juanca.med@gmail.com>
2016-10-14 16:09:02 -0700

    Export all components and fix references in example files

--- react-tree-entry.js
+++ react-tree-entry.js
@@ -1,3 +1,9 @@
 module.exports = {
-  Grid: require('./lib/grid.js')
+  BodyCell: require('./lib/body-cell'),
+  BodyRowCells: require('./lib/body-row-cells'),
+  BodyRow: require('./lib/body-row'),
+  Body: require('./lib/body'),
+  Grid: require('./lib/grid'),
+  HeaderCell: require('./lib/header-cell'),
+  HeaderRow: require('./lib/header-row'),
 };
