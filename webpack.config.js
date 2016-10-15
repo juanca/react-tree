2f19cc9d9786cc99e267f2669eecc789f7c72da1
Juan Carlos Medina <juanca.med@gmail.com>
2016-10-14 17:20:53 -0700

    Rename files with JSX extension

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,12 @@ module.exports = {
       test: /\.js$/,
       exclude: /(node_modules)/,
       loader: 'babel-loader',
-      query: {
-        presets: ['es2015', 'react']
-      }
+      query: { presets: ['es2015'] }
+    }, {
+      test: /\.jsx$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: { presets: ['es2015', 'react'] }
     }],
   },
 
